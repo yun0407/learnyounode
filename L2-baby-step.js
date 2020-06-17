c0fa40f55526387d7b31e51d16fc624fe8f6d94b
yun0407 <star860407@gmail.com>
2020-06-17 11:35:00 +0800

    Add baby-step.js

---
+++ L2-baby-step.js
@@ -0,0 +1,9 @@
+
+let sum=0; //宣告 sum 這個變數 等於 0，加總從0開始
+array = process.argv.length
+    for (i = 2; i < array ; i++) {
+        sum += Number(process.argv[i]);
+             //把字串轉成數字
+}
+
+console.log(sum);
